43d8bc962b5a1e706b72afd7e62af69482743683
khainguyen98 <91797066+khainguyen98@users.noreply.github.com>
2021-10-03 00:39:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'hà xinh đẹp à!',
+    introDesc: `Anh tạo cái này để hỏi em, nhớ trả lời thật đó nhé :DDD`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    title: 'Nếu như em không trả lời mà thoát ra thì tức là mai em muốn đi ăn bánh gà cùng anh nhá :V 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnYes: 'Oki em đồng ý <33',
+    btnNo: 'Anh bị ngáo à :3',
+    question:'Nghỉ thui hà ơi mai cố gắng nha mai gặp anh ',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: 'Oki vậy mai anh qua nhaaaa <33333333'',
+    mess: 'Anh biết mà 🥰. mai em phải cố gắng phỏng vấn oki nhé 😘😘',
+    messDesc: 'vậy mai anh qua đưa em đi luôn.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https:https://www.facebook.com/khainguyen27/
 }
