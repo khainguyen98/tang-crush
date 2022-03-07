63f0a2ccdf15450bbb5bd493eee3b3290b6b40b1
khainguyen98 <91797066+khainguyen98@users.noreply.github.com>
2022-03-07 20:18:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,16 @@
 const CONFIG = {
-    introTitle: 'bé hà xinh đẹp à!',
-    introDesc: `Ai cũng có thể yêu em lúc mặt trời
-    tỏa nắng nhưng bão tố mới là nơi tìm
-    ra được người thực sự quan tâm em :> `,
+    introTitle: ' thanh xinh đẹp à!',
+    introDesc: `vẫn là anh khải nè `,
     btnIntro: 'Vâng hihi',
-    title: 'Cuộc đời này không giống như một bộ phim cho mình nhiều lần gặp gỡ 😙',
-    desc: 'Để cho duyên mình được lỡ hà có đồng ý làm nữ chính trong cuộc đời anh không? ',
-    btnYes: 'Vâng em đồng ý <33',
-    btnNo: 'Không, em chọn Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'mai anh qua đưa cháo cho em ăn nha bé iu <3.',
+    title: 'Nay 8/3 nè biết hôngggg',
+    desc: 'Chúc em 8/3 thât vui vẻ xinh đẹp và hanh phúc nhaaa ? ',
+    btnYes: 'Vâng em biết rùiiii <33',
+    btnNo: 'Không, em không thích :3',
+    question:'Ngoan đấyyy thế hôm nay sẽ là 1 ngày tuyệt vời với emmm',
+    btnReply: 'Gửi cho anh',
+    reply: 'ok anh ơiii em biết mà hihihi',
+    mess: 'Anh biết mà ',
+    messDesc: 'cô gái nay làm về mệt rùiii ngủ sớm giữ sức mai đi làm na.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https:https://www.facebook.com/khainguyen27/
 }
